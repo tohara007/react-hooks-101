78cfd429f0c243aef9c6d46c1024586be1f4eaf1
Taiki Ohara <taikiohara@example.com>
2021-03-10 22:19:35 +0900

    implement component using multiple states

--- src/App.js
+++ src/App.js
@@ -1,54 +1,32 @@
 import React, { useState } from 'react'
 
-const App = () => {
-  const [count, setCount] = useState(0)
+const App = props => {
 
-  const increment = () => setCount(count + 1)
-  const decrement = () => setCount(count - 1)
+  const [name, setName] = useState(props.name)
+  const [price, setPrice] = useState(props.price)
 
-  const increment2 = () => setCount(precount => precount + 1)
-  const decrement2 = () => setCount(precount => precount - 1)
-
-  const reset = () => setCount(0)
-  const double = () => setCount(precount => precount * 2)
-  // 自前で考えたver（合っていた）
-  // const onethird = () => {
-  //   if (count % 3 === 0) {
-  //     setCount(count / 3)
-  //   }
-  // }
-  // 答えver
-  // const onethird = () => setCount(precount => {
-  //   if (precount % 3 === 0) {
-  //     return precount / 3
-  //   } else {
-  //     return precount
-  //   }
-  // })
-  const onethird = () => setCount(precount => 
-    precount % 3 === 0 ? precount / 3 : precount
-  )
+  const increment = () => setPrice(price + 1)
+  const decrement = () => setPrice(price - 1)
+  const reset = () => {
+    setPrice(props.price)
+    setName(props.name)
+  }
+  const changeName = (value) => setName(value)
 
   return (
     <>
-      <div>
-        <p>count: {count}</p>
-      </div>
-      <div>
-        <button onClick={increment}>+1</button>
-        <button onClick={decrement}>-1</button>
-      </div>
-      <div>
-        <button onClick={increment2}>+1</button>
-        <button onClick={decrement2}>-1</button>
-      </div>
-      <div>
-        <button onClick={reset}>Reset</button>
-        <button onClick={double}>x2</button>
-        <button onClick={onethird}>3の倍数の時だけ3で割る</button>
-      </div>
+    <p>現在の{name}は、{price}円です</p>
+    <button onClick={increment}>+1</button>
+    <button onClick={decrement}>-1</button>
+    <button onClick={reset}>Reset</button>
+    <input value={name} onChange={e => changeName(e.target.value)}/>
     </>
   )
+}
+
+App.defaultProps = {
+  name: '',
+  price: 1000
 }
 
 export default App
